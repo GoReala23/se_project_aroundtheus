a514f70a880e620b0f03af95bb187bc083cad6b4
Hassan <porterh1991@gmail.com>
2023-10-07 19:48:00 -0400

    update

--- scripts/validation.js
+++ scripts/validation.js
@@ -32,20 +32,18 @@ function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
     submitButton.disabled = true;
     return;
   }
-
-  submitButton.classList.remove(inactiveButtonClass);
-  submitButton.disabled = false;
 }
 
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
 
-  const submitButton = formEl.querySelector(".popup__button");
+  const submitButton = formEl.querySelector(options.submitButtonSelector);
 
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
-      console.log(inputEl);
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton);
     });
   });
 }
@@ -58,16 +56,20 @@ function enableValidation(options) {
     });
 
     setEventListeners(formEl, options);
+    checkInputValidity(formEl, options);
   });
 }
 
 const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__span_opened",
 };
 
 enableValidation(config);
+setEventListeners();
+showInputError();
+toggleButtonState();
